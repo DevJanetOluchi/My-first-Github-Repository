7616707ac81f8bc590eb79de1920746837b28c48
Janet Paul <oluchijanet7@gmail.com>
2022-07-15 20:53:01 +0100

    Challenge 1 and 2 completed

--- src/assignment.js
+++ src/assignment.js
@@ -8,28 +8,49 @@ const assignment = {};
  * Challenge - 1
  * 
  * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
+ * If you did Challenge - 1, 
  * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
+ //* @param {Array} arrayOfNumbers the array of numbers to sum
+ //* @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+const updateBlock =(value) =>{
+};
+
+const numbers = [
+    1, 5, 10, 20, 30, 40, 50, 60,
+];
+
+//function sumOfNumbers(arrayOfNumbers) {
+  //  return arrayOfNumbers.length;
+//}
+const sum = [10, 20, 30].reduce(add, 0); // with initial value to avoid when the array is empty
+
+function add(accumulator, a) {
+  return accumulator + a;
 }
+
+console.log(sum); 
 // assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
  * 
  * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
+ * If you did Challenge - 2, 
  * 
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
+ * //@returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
+//function countEvenNumbers(arrayOfNumbers) {
+    //return arrayOfNumbers.length;
+//}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const even = arr.filter(number => {
+  return number % 2 === 0;
+});
+
+console.log(even);
 // assignment.countEvenNumbers = countEvenNumbers;
 
 /**
